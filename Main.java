181b104f6e6fffe83e96734dc332b7407e1d46f6
Qourle <80998672+Qourle@users.noreply.github.com>
2023-02-10 15:50:43 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    try(Scanner input = new Scanner(System.in)) {
+      int testCases = Integer.parseInt(input.nextLine());
+      for(int testcase = 0; testcase < testCases; testcase++) {
+        int lines = input.nextInt();
+        String output = "";
+        for(int row = 0; row < lines; row++) {
+          output = "";
+          for(int column = 0; column < lines; column++) {
+            output += "# ";
+          }
+          System.out.println(output);
+        }
+      }
+    }
+  }
+}
